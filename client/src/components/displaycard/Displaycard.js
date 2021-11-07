63092cfcc19d85dd73798502ce133913942b8f11
Norman Li <normanjli@gmail.com>
2021-11-07 16:51:04 -0700

    fixed display card

--- client/src/components/displaycard/Displaycard.js
+++ client/src/components/displaycard/Displaycard.js
@@ -8,7 +8,7 @@ const Displaycard = ({drink}) => {
             <a href={url} rel="noreferrer noopener" target='_blank'>
             <img className = "drink-img" src={strDrinkThumb} alt={strDrink}/>
             </a>
-            <h2 className="drink">{strDrink}</h2>
+            <h2 className="drinkName">{strDrink}</h2>
         </div>
     );
 };
