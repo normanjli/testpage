927cf50257a0185d62a788430b70558b8e0e75c2
Norman Li <normanjli@gmail.com>
2021-11-06 22:06:59 -0600

    made search bring up an array of driunks to the sc

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Button from './components/button/Button'
 import Search from './components/search/search'
 import React, {useState} from 'react'
 import Displaycard from './components/displaycard/Displaycard';
+
 //TODO useeffect hook
+
 function App() {
   const [inputValue, setInputValue] = useState('')
   const [drink, setDrink] = useState(``)
-  let drinkCard
+  let drinkCard =[]
   const onChange=(event)=>{
     setInputValue(event.target.value)
   }
@@ -17,30 +19,30 @@ function App() {
     .then(res =>{
         let {drinks} = res.data
         let item = drinks.pop()
-        console.log(item)
         setDrink(item)
     })
-  }
-  if(drink.length ===0){
-  }else{
-    drinkCard = <Displaycard drink={drink}/>
   }
   const onSubmit = (event)=>{
     event.preventDefault()
     let searchTerm = inputValue.replace(` `, `+`)
-    console.log(searchTerm)
     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(res=>{
       let {drinks} = res.data
-      let item = drinks[Math.floor(Math.random()*(drinks.length-1))]
-      console.log(item)
-      setDrink(item)
+      setDrink(drinks)
     })
+  }
+  console.log(drink)
+  if(drink.length === undefined){
+    drinkCard.push(<Displaycard drink={drink}/>)
+  }else{
+    for(let i=0;i<drink.length;i++){
+      drinkCard.push(<Displaycard drink={drink[i]}/>)
+    }
   }
   return (
     <>
     <Search inputValue={inputValue} onChange={onChange} onSubmit={onSubmit}/>
     <Button onClick={drinkClick} id={`test`} text={`Big Drink button`}/>
-    {drinkCard}
+    <div className='drink-container'>{drinkCard}</div>
     </>
   );
 }
