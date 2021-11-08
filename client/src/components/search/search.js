640e6e3d928601cadf0a87e076fab5c0737dd183
Norman Li <normanjli@gmail.com>
2021-11-07 17:44:59 -0700

    fixed display message

--- client/src/components/search/search.js
+++ client/src/components/search/search.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const Search = ({inputValue, onChange, placeholder, key, onSubmit}) => {
+const Search = ({inputValue, onChange, placeholder, key, onSubmit,sent}) => {
     let searchingFor = ''
     if(inputValue===``){
         
-    }else{
+    }else if(sent === false){
         searchingFor = `Searching for ${inputValue}`
+    }else if(sent === true){
+        searchingFor = `${inputValue} did not return and results. Try gin and tonic.`
     }
     return (
         <form className='searchdrink' onSubmit={onSubmit}>
-            <input placeholder={placeholder} value ={inputValue} onChange = {onChange} key = {key}></input>
+            <input placeholder={placeholder} onChange = {onChange} key = {key}></input>
             <input type='submit'value='Search for drink'></input>
             <p>{searchingFor}</p>
         </form>
--- client/src/routes/Drinks.js
+++ client/src/routes/Drinks.js
@@ -5,46 +5,47 @@ import Search from '../components/search/search';
 import Button from '../components/button/Button'
 
 const Drinks = () => {
-    const [inputValue, setInputValue] = useState('')
-    const [drink, setDrink] = useState(``)
-    let drinkCard =[]
-    let message = ``
-    const onChange=(event)=>{
-      setInputValue(event.target.value)
-    }
-    const drinkClick = () =>{
-      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-      .then(res =>{
-          let {drinks} = res.data
-          let item = drinks.pop()
-          setDrink(item)
-      })
-    }
-    const onSubmit = (event)=>{
-      event.preventDefault()
-      let searchTerm = inputValue.replace(` `, `+`)
-      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(res=>{
+  const [inputValue, setInputValue] = useState('')
+  const [drink, setDrink] = useState(``)
+  let drinkCard =[]
+  const [sent, setSent] = useState(false)
+  const onChange=(event)=>{
+    setInputValue(event.target.value)
+    setSent(false)
+  }
+  const drinkClick = () =>{
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+    .then(res =>{
         let {drinks} = res.data
-        setDrink(drinks)
-      }).catch(err=>console.log(err))
+        let item = drinks.pop()
+        setDrink(item)
+    })
+  }
+  const onSubmit = (event)=>{
+    event.preventDefault()
+    let searchTerm = inputValue.replace(` `, `+`)
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(res=>{
+      let {drinks} = res.data
+      setDrink(drinks)
+      drink===null?setSent(true):setSent(false)
+    }).catch(err=>console.log(err))
+  }
+  if(drink===null){
+  }
+  else if(drink.length === undefined){
+    drinkCard.push(<Displaycard drink={drink}/>)
+  }else{
+    for(let i=0;i<drink.length;i++){
+      drinkCard.push(<Displaycard drink={drink[i]}/>)
     }
-    if(drink===null){
-      message = `${inputValue} did not return any results try gin and tonic`
-    }
-    else if(drink.length === undefined){
-      drinkCard.push(<Displaycard drink={drink}/>)
-    }else{
-      for(let i=0;i<drink.length;i++){
-        drinkCard.push(<Displaycard drink={drink[i]}/>)
-      }
-    }
-    return (
-      <>
-      <Search inputValue={drink===null?message:inputValue} onChange={onChange} onSubmit={onSubmit}/>
-      <Button onClick={drinkClick} id={`test`} text={`Big Drink button`}/>
-      <div className='drink-container'>{drinkCard}</div>
-      </>
-    )
+  }
+  return (
+    <>
+    <Search inputValue={inputValue} onChange={onChange} onSubmit={onSubmit} sent={sent} />
+    <Button onClick={drinkClick} id={`test`} text={`Big Drink button`}/>
+    <div className='drink-container'>{drinkCard}</div>
+    </>
+  )
 };
 
 export default Drinks;
