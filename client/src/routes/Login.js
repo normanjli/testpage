050dab83b3b612cfc1e11e0008b0ced97efca115
Norman Li <normanjli@gmail.com>
2021-11-07 17:26:04 -0700

    express sever added, routes, and other stuff

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const Login = () => {
+    // const [login, setLogin] = useState({
+    //     username: '',
+    //     password: ''
+    // })
+
     return (
         <div>
             <form className='login'>
                 <div>
                     <label>Username </label>
-                    <input type='text' placeholder='Username'></input>
+                    <input id='username' type='text' placeholder='Username'></input>
                 </div>
                 <div>
                     <label>Password </label>
-                    <input type='password' placeholder='Password'></input>
+                    <input id='password' type='password' placeholder='Password'></input>
                 </div>
                 <div className='loginbtns'><button>Login</button><button>Create User</button></div>
             </form>
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require(`express`)
+const path = require('path')
+
 const app = express()
 
-const port = process.env.PORT || 5000
+app.use(express.json())
 
+const port = process.env.PORT || 4000
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'../public/index.html'))
+// })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
