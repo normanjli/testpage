f61f2aaa1b974c9b6dc97db8b2dec938bd3ee874
Norman Li <normanjli@gmail.com>
2021-11-07 17:38:07 -0700

    server stuff

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 const port = process.env.PORT || 4000
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.join(__dirname,'../public/index.html'))
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
