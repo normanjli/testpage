0700e9ec03a6e0dd5d5aed62574b4fdfe0389d97
Norman Li <normanjli@gmail.com>
2021-11-09 14:31:10 -0700

    changes to styling

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h3>Here you can view random drinks queried from the <a href="https://www.thecocktaildb.com" target='_blank' rel="noreferrer">cocktail api</a>. <br/> You will be able to make an account, save favorites, review them and keep notes on the drink</h3>
+        <div style={{width:'50vw', marginTop:'5em'}}>
+            <h2>Here you can view random drinks queried from the <a href="https://www.thecocktaildb.com" target='_blank' rel="noreferrer">cocktail api</a>. You will be able to make an account, save favorites, review them and keep notes on the drink</h2>
         </div>
     );
 };
