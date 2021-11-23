9736639f44e00982a975c79e197508b446dc3002
Norman Li <normanjli@gmail.com>
2021-11-23 10:48:31 -0700

    test

--- config/config.js
+++ config/config.js
@@ -22,12 +22,12 @@ module.exports = {
   },
   production: {
     use_env_variable: DATABASE_URL,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-      }
-    },
+    // dialect: 'postgres',
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    //   }
+    // },
   },
 };
